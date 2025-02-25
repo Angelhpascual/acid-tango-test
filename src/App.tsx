c7d762f1d0aa18cb415e340290008b2494344ce0
Angelhpascual <Angelhpascual@gmail.com>
2025-02-25 13:17:29 +0100

    Added colors and typo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold text-amber-300">Hello World</h1>
+    <div className="h-screen w-screen bg-[var(--accent)]">
+      <h1 className="text-4xl font-primary text-white">Hello World</h1>
     </div>
   )
 }
