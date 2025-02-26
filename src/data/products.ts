13fe8a69179188ff7325f99e56ad158e3bbaf5db
Angelhpascual <Angelhpascual@gmail.com>
2025-02-26 13:03:40 +0100

    added product images and product hardcoded data

---
+++ src/data/products.ts
@@ -0,0 +1,26 @@
+import { nanoid } from "nanoid"
+import { Product } from "../types/product"
+
+export const products: Product[] = [
+  {
+    id: nanoid(),
+    name: "Camiseta Negra",
+    price: 20,
+    description: "Camiseta de algodón 100% de color negra",
+    image: "/images/products/shirt.webp",
+  },
+  {
+    id: nanoid(),
+    name: "Zapatillas Blancas",
+    price: 60,
+    description: "Zapatillas de deporte de color blanco",
+    image: "/images/products/sneakers.webp",
+  },
+  {
+    id: nanoid(),
+    name: "Mochila Negra",
+    price: 40,
+    description: "Mochila de color negra con varios compartimentos",
+    image: "/images/products/bag.webp",
+  },
+]
